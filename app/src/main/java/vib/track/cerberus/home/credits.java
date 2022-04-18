2136a9b8e9d67b5d2d97fd9d6aa33831ec09ad62
Vinson Mach <vinson.mach@mavs.uta.edu>
2022-04-18 11:38:24 -0500

    Reworked UI, Navigation v1
    - half done UI and Navigation overhaul

---
+++ app/src/main/java/vib/track/cerberus/home/credits.java
@@ -0,0 +1,64 @@
+package vib.track.cerberus.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import vib.track.cerberus.R;
+import vib.track.cerberus.history.HistoryList;
+import vib.track.cerberus.home.HomepageActivity;
+import vib.track.cerberus.home.SecondFragment;
+
+public class credits extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_second);
+
+        // assign variables
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    public void clickMenu(View view){
+        // open drawer
+        HomepageActivity.openDrawer(drawerLayout);
+    }
+
+    public void clickLogo(View view){
+        // close drawer
+        HomepageActivity.closeDrawer(drawerLayout);
+    }
+
+    public void clickHistory(View view){
+        // redirect to history
+        HomepageActivity.redirectActivity(this, HistoryList.class);
+    }
+
+    public void clickSettings(View view){
+        // recreate settings activity
+        HomepageActivity.redirectActivity(this, SecondFragment.class);
+    }
+
+    public void clickCredits(View view){
+        // redirect to credits
+        recreate();
+    }
+
+    public void clickLogout(View view){
+        // redirect to history
+        HomepageActivity.logout(this);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        // close drawer
+        HomepageActivity.closeDrawer(drawerLayout);
+    }
+}
