cbff2a97454fda901c4db5cc8b4a4464a7df5691
Katie Baumann <baumannkeb@gmail.com>
2022-04-18 12:15:02 -0500

    Added Paul's files

--- app/src/main/java/vib/track/cerberus/data/SingleEvent.java
+++ app/src/main/java/vib/track/cerberus/data/SingleEvent.java
@@ -1,2 +1,19 @@
-package vib.track.cerberus.data;public class SingleEvent {
+package vib.track.cerberus.data;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class SingleEvent {
+    @SerializedName("statusCode")
+    public int statusCode;
+    @SerializedName("message")
+    public String message;
+    @SerializedName("body")
+    public List<HistoryData> body;
+
+    @Override
+    public String toString() {
+        return "TestItem{" + "body=" + body + "}";
+    }
 }
--- app/src/main/java/vib/track/cerberus/history/HistoryActivity.java
+++ app/src/main/java/vib/track/cerberus/history/HistoryActivity.java
@@ -1,2 +1,68 @@
-package vib.track.cerberus.history;public class HistoryActivity {
+package vib.track.cerberus.history;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import vib.track.cerberus.R;
+import vib.track.cerberus.data.HistoryData;
+import vib.track.cerberus.data.SingleEvent;
+import vib.track.cerberus.network.RetrofitClient;
+import vib.track.cerberus.network.ServiceApi;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    SingleEvent dataList;
+    List<HistoryData> historyDataInfo;
+
+    RecyclerView recyclerView;
+    RecyclerAdapter recyclerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        historyDataInfo = new ArrayList<>();
+        recyclerView = findViewById(R.id.recyclerView);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ServiceApi serviceApi = RetrofitClient.getClient().create(ServiceApi.class);
+
+        Call<SingleEvent> call = serviceApi.getData();
+
+        call.enqueue(new Callback<SingleEvent>() {
+            @Override
+            public void onResponse(Call<SingleEvent> call, Response<SingleEvent> response) {
+
+                dataList = response.body();
+
+                Log.d("MainActivity", dataList.toString());
+
+                historyDataInfo = dataList.body;
+
+                recyclerAdapter = new RecyclerAdapter(getApplicationContext(), historyDataInfo);
+                recyclerView.setAdapter(recyclerAdapter);
+
+            }
+
+
+            @Override
+            public void onFailure(Call<SingleEvent> call, Throwable t) {
+
+                Log.d("MainActivity", t.toString());
+            }
+        });
+    }
 }
