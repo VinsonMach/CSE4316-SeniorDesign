fa55660ee729dc2ca1f1b368493fc8757afd3b7e
Matthew-Blount <59977242+Matthew-Blount@users.noreply.github.com>
2022-04-23 17:31:17 -0500

    Add files via upload

--- wristband.ino
+++ wristband.ino
@@ -1,4 +1,5 @@
 #include <ArduinoBLE.h>
+#include <TimedAction.h>
 
 int RED_PIN=9;
 int BLUE_PIN=7;
@@ -10,8 +11,10 @@ const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
 const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
 BLEService gestureService(deviceServiceUuid); 
 
-BLEByteCharacteristic gestureCharacteristic(deviceServiceCharacteristicUuid, BLERead | BLEWrite);
+BLEStringCharacteristic gestureCharacteristic(deviceServiceCharacteristicUuid, BLERead | BLEWrite, 128);
 int ledeez[3];
+
+byte queue[10][2];
 
 void setup(){
     pinMode(RED_PIN, OUTPUT);
@@ -19,6 +22,7 @@ void setup(){
     pinMode(GREEN_PIN, OUTPUT);
     pinMode(VIB_PIN, OUTPUT);
     pinMode(AUDIO_IN, INPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
     ledeez[0] = 9;
     ledeez[1] = 7;
     ledeez[2] = 5;
@@ -31,14 +35,50 @@ void setup(){
   BLE.setAdvertisedService(gestureService);
   gestureService.addCharacteristic(gestureCharacteristic);
   BLE.addService(gestureService);
-  gestureCharacteristic.writeValue(-1);
+  gestureCharacteristic.writeValue("");
   BLE.advertise();
 }
 
+void insert(byte b1, byte b2){
+  for(int i = 0; i < 10; i++){
+    if(queue[i][0] == 0){
+      queue[i][0] = b1;
+      queue[i][1] = b2;
+      break;
+    }
+  }
+}
+
+void shift(byte arr[10][2]){
+  for(int i = 0; i < 9; i++){
+    arr[i][0] = arr[i+1][0];
+    arr[i][1] = arr[i+1][1];
+  }
+  arr[9][0] = 0;
+  arr[9][1] = 0;
+}
+
+void vibrate(){
+  if(queue[0][0] != 0){
+    Serial.println("IN HERE");
+    for(int i = 0; i < queue[0][1]; i++){
+      Serial.println("LOOP");
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(queue[0][0]*10);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(25);
+    }
+    shift(queue);
+  }
+}
+
+TimedAction vibrateThread = TimedAction(10,vibrate);
+
 void loop(){
+    vibrateThread.check();
     BLEDevice central = BLE.central();
     Serial.println("- Discovering central device...");
-    delay(5000);
+    delay(500);
     if (central) {
 
     Serial.println("* Connected to central device!");
@@ -51,15 +91,28 @@ void loop(){
 
 
     while (central.connected()) {
-      int vol = analogRead(AUDIO_IN);
-      Serial.print(vol);
+      vibrateThread.check();
+      if (gestureCharacteristic.written()) {
+        Serial.println("WRITE");
+        String read_in = gestureCharacteristic.value();
+        char temp[3];
+        read_in.substring(0, read_in.indexOf(" ")).toCharArray(temp, 3);
+        byte byte1 = atoi(temp);
+        read_in.substring(read_in.indexOf(" ")+1, read_in.length()).toCharArray(temp, 3);
+        byte byte2 = atoi(temp);
+        Serial.println(byte2);
+        insert(byte1, byte2);
+      }
+      int vol = 10;//analogRead(AUDIO_IN);
+      //Serial.print(vol);
       int ledLevel = map(abs(vol-540), 0, (1024-540), 0, 3);
       for(int i = 0; i < 3; i++){
+        vibrateThread.check();
         if(i < ledLevel){
-            analogWrite(ledeez[i], 128);
+            //analogWrite(ledeez[i], 128);
         }
         else{
-            analogWrite(ledeez[i], 0);
+            //analogWrite(ledeez[i], 0);
         }
       }
     }
