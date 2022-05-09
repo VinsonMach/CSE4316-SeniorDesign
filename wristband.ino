91f1a87649ede2b7b2f501068aecd012d61d321a
Matthew-Blount <59977242+Matthew-Blount@users.noreply.github.com>
2022-05-09 11:30:29 -0500

    Add files via upload

--- wristband.ino
+++ wristband.ino
@@ -2,9 +2,10 @@
 #include <TimedAction.h>
 
 int RED_PIN=9;
+int GREEN_PIN=8;
 int BLUE_PIN=7;
-int GREEN_PIN=5;
-int VIB_PIN=3;
+int VIB_PIN=10;
+int VIB_PIN2 = 6;
 int AUDIO_IN = A0;
 const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
 
@@ -12,7 +13,6 @@ const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1
 BLEService gestureService(deviceServiceUuid); 
 
 BLEStringCharacteristic gestureCharacteristic(deviceServiceCharacteristicUuid, BLERead | BLEWrite, 128);
-int ledeez[3];
 
 byte queue[10][2];
 
@@ -21,16 +21,14 @@ void setup(){
     pinMode(BLUE_PIN, OUTPUT);
     pinMode(GREEN_PIN, OUTPUT);
     pinMode(VIB_PIN, OUTPUT);
+    pinMode(VIB_PIN2, OUTPUT);
     pinMode(AUDIO_IN, INPUT);
     pinMode(LED_BUILTIN, OUTPUT);
-    ledeez[0] = 9;
-    ledeez[1] = 7;
-    ledeez[2] = 5;
-    Serial.begin(9600);
     if (!BLE.begin()) {
       Serial.println("- Starting Bluetooth® Low Energy module failed!");
       while (1);
   }
+  
   BLE.setLocalName("Wristband");
   BLE.setAdvertisedService(gestureService);
   gestureService.addCharacteristic(gestureCharacteristic);
@@ -60,13 +58,15 @@ void shift(byte arr[10][2]){
 
 void vibrate(){
   if(queue[0][0] != 0){
-    Serial.println("IN HERE");
     for(int i = 0; i < queue[0][1]; i++){
-      Serial.println("LOOP");
+      digitalWrite(VIB_PIN, HIGH);
+      digitalWrite(VIB_PIN2, HIGH);
       digitalWrite(LED_BUILTIN, HIGH);
-      delay(queue[0][0]*10);
+      delay(queue[0][0]*100);
+      digitalWrite(VIB_PIN, LOW);
+      digitalWrite(VIB_PIN2, LOW);
       digitalWrite(LED_BUILTIN, LOW);
-      delay(25);
+      delay(100);
     }
     shift(queue);
   }
@@ -77,49 +77,43 @@ TimedAction vibrateThread = TimedAction(10,vibrate);
 void loop(){
     vibrateThread.check();
     BLEDevice central = BLE.central();
-    Serial.println("- Discovering central device...");
+    digitalWrite(BLUE_PIN, HIGH);
+    digitalWrite(GREEN_PIN, LOW);
+    digitalWrite(RED_PIN, LOW);
     delay(500);
+    digitalWrite(BLUE_PIN, LOW);
     if (central) {
-
-    Serial.println("* Connected to central device!");
-
-    Serial.print("* Device MAC address: ");
-
-    Serial.println(central.address());
-
-    Serial.println(" ");
 
 
     while (central.connected()) {
       vibrateThread.check();
       if (gestureCharacteristic.written()) {
-        Serial.println("WRITE");
         String read_in = gestureCharacteristic.value();
         char temp[3];
         read_in.substring(0, read_in.indexOf(" ")).toCharArray(temp, 3);
         byte byte1 = atoi(temp);
         read_in.substring(read_in.indexOf(" ")+1, read_in.length()).toCharArray(temp, 3);
         byte byte2 = atoi(temp);
-        Serial.println(byte2);
         insert(byte1, byte2);
       }
-      int vol = 10;//analogRead(AUDIO_IN);
-      //Serial.print(vol);
-      int ledLevel = map(abs(vol-540), 0, (1024-540), 0, 3);
-      for(int i = 0; i < 3; i++){
-        vibrateThread.check();
-        if(i < ledLevel){
-            //analogWrite(ledeez[i], 128);
-        }
-        else{
-            //analogWrite(ledeez[i], 0);
-        }
+      int vol = analogRead(AUDIO_IN);
+      if(vol < 50){
+        digitalWrite(GREEN_PIN, HIGH);
+        digitalWrite(RED_PIN, LOW);
+        digitalWrite(BLUE_PIN, LOW);
+      }
+      else if(vol < 512){
+        digitalWrite(GREEN_PIN, HIGH);
+        digitalWrite(RED_PIN, HIGH);
+        digitalWrite(BLUE_PIN, LOW);
+      }
+      else{
+        digitalWrite(GREEN_PIN, LOW);
+        digitalWrite(RED_PIN, HIGH);
+        digitalWrite(BLUE_PIN, LOW);
       }
     }
 
-    
-
-    Serial.println("* Disconnected to central device!");
 
   }
 
